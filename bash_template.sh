a966f74611a26cf6a6fe57571ac4a4fceb1ecd9c
fbouynot <felixbouynot@gmail.com>
2023-04-12 14:46:56 +0200

    change template logic

--- bash_template.sh
+++ bash_template.sh
@@ -18,6 +18,7 @@
 # -u: This will make the script fail, when accessing an unset variable
 # -o pipefail: This will ensure that a pipeline command is treated as failed, even if one command in the pipeline fails
 set -euo pipefail
+
 # Enable debug mode by running your script as TRACE=1 ./script.sh instead of ./script.sh
 if [[ "${TRACE-0}" == "1" ]]
 then
@@ -26,21 +27,19 @@ fi
 
 # Define constants
 readonly PROGNAME="${0##*/}"
-readonly VERSION='1.0.0'
-readonly DEFAULT_LOG_FILE=${PROGNAME}.log
+readonly VERSION='1.1.0'
 readonly DEFAULT_VERBOSITY=0
-readonly DEFAULT_QUIET=0
+readonly DEFAULT_MESSAGE="Hello, World!"
 
 # Help function: print the help message
 help() {
     cat << EOF
-Usage: ${PROGNAME} [ { -l | --logfile } <logfile> ] [-Vhqv]
-Install laravel and the web stack on GNU/linux.
+Usage: ${PROGNAME} [-Vhqv]
+Print "Hello, World!"
 
 Options:
     -h    --help                                                     Print this message and exit
-    -l    --logfile          <string>                                The chosen logfile (default: ${DEFAULT_LOG_FILE})
-    -q    --quiet                                                    No output at all
+    -m    --message                                                  Message to print (default: ${DEFAULT_MESSAGE})
     -v    --verbose                                                  Print the verbose output
     -V    --version                                                  Print the version and exit
 EOF
@@ -57,6 +56,12 @@ EOF
 exit 2
 }
 
+# Display help message if there is no parameter
+# if [[ $# -eq 0 ]]
+# then
+#     help
+# fi
+
 # Deal with arguments
 while [[ $# -gt 0 ]]
 do
@@ -66,18 +71,12 @@ do
         -h|--help)
             help
             ;;
-        -l|--logfile)
-            export WORLD="${2}"
-            shift # consume -l
-            ;;
-        -q|--quiet)
-            export QUIET=1
+        -m|--message)
+            MESSAGE="${2}"
+            shift # consume -m
             ;;
         -v|--verbose)
             export VERBOSITY=1
-            ;;
-        -vv)
-            export VERBOSITY=2
             ;;
         -V|--version)
             version
@@ -89,60 +88,11 @@ do
 done
 
 # Set defaults if no options specified
-LOG_FILE="${LOG_FILE:-$DEFAULT_LOG_FILE}"
 VERBOSITY="${VERBOSITY:-$DEFAULT_VERBOSITY}"
-QUIET="${QUIET:-$DEFAULT_QUIET}"
+MESSAGE="${MESSAGE:-$DEFAULT_MESSAGE}"
 
 # Change directory to base script directory
 cd "$(dirname "${0}")"
-
-# Function to log and run the command
-log_and_run() {
-    # Explicitly define our arguments
-    local ARG_TEXT ARG_COMMAND EXIT_CODE OUTPUT
-
-    ARG_TEXT=$1
-    ARG_COMMAND=$2
-
-    # Print nothing if quiet option
-    if [[ "${QUIET}" == "0" ]]
-    then
-        printf "%-50s" "${ARG_TEXT}"
-    fi
-    echo "${ARG_TEXT}" >> "${LOG_FILE}"
-    # Print and log stderr if verbose, log stderr if not
-    if [[ "${VERBOSITY}" != "0" ]]
-    then
-        set +o pipefail
-        eval "${ARG_COMMAND}" | tee -a "${LOG_FILE}"
-        EXIT_CODE=${PIPESTATUS[0]}
-        set -o pipefail
-    else
-        set +e
-        eval "${ARG_COMMAND}" > /dev/null 2>> "${LOG_FILE}"
-        EXIT_CODE=$?
-        set -e
-    fi
-    echo "Returned: ${EXIT_CODE}" >> "${LOG_FILE}"
-
-    # Print OK if the command ran successfully
-    # Print FAIL otherwise (non-zero exit code)
-    # Print nothing if option quiet set
-    if [[ "${EXIT_CODE}" == "0" ]]
-    then
-        if [[ "${QUIET}" == "0" ]]
-        then
-            printf " \\033[0;32mOK\\033[0m\\n"
-        fi
-    else
-        if [[ "${QUIET}" == "0" ]]
-        then
-            printf " \\033[0;31mFAIL\\033[0m\\n"
-        fi
-    fi
-
-    return "${EXIT_CODE}"
-}
 
 # Check root permissions
 check_root() {
@@ -150,7 +100,7 @@ check_root() {
     if [ "${EUID}" -ne 0 ]
     then
         printf 'E: please run as root\n' >&2
-        return 3
+        exit 3
     fi
 
     return 0
@@ -158,9 +108,20 @@ check_root() {
 
 # Main function
 main() {
-    echo '' > "${LOG_FILE}" || (echo "E: Cannot write the log file: ${LOG_FILE}" >&2 && exit 4)
-    log_and_run 'Checking permissions' 'check_root'
-    log_and_run 'Print Hello World!' 'echo "Hello World!"'
+    # Check for root permissions
+    check_root
+    # Print Hello World! on stout
+    echo ${MESSAGE}
+
+    # Compress program with xz
+    case "${VERBOSITY}" in
+        1)
+            tar cvvJf archive.xz "${PROGNAME}"
+            ;;
+        *)
+            tar cJf archive.xz "${PROGNAME}" > /dev/null 2>&1
+            ;;
+    esac
 
     exit 0
 }
