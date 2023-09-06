06fa0c7a2d69c0e9a45c1a90578dd80ba3cfd9ad
Félix Bouynot <felixbouynot@gmail.com>
2023-09-06 15:33:34 +0200

    Update laravel_install.sh

--- laravel_install.sh
+++ laravel_install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2317,SC2154
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
